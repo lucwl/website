7a87606e9166a9647e852bbf5cdd5866e89f6e1e
Luc Wallace <144689311+luc-wallace@users.noreply.github.com>
2023-09-13 22:50:12 +0100

    Add experience section and fix hyperlinks

--- index.js
+++ index.js
@@ -5,19 +5,20 @@ fetch("https://api.github.com/users/thefakequake/repos")
   .then((res) => res.json())
   .then((data) => {
     data.sort((a, b) => a.stargazers_count - b.stargazers_count).reverse()
-    console.log(data)
 
     let html = ""
-    for (const repo of data.slice(0, 5)) {
+    for (const repo of data.slice(0, 8)) {
       html += /*html*/ `
-        <span>
+        <a href="${repo.owner.html_url}" class="author-link">
           <img src="${repo.owner.avatar_url}" />
-          <a href="${repo.owner.html_url}">${repo.owner.login}</a>
-        </span>
+          ${repo.owner.login}
+        </a>
         <a href="${repo.svn_url}">${repo.name}</a>
         <p>${repo.stargazers_count} stars</p>
       `
     }
 
     projectsContainer.innerHTML = DOMPurify.sanitize(html)
+  }).catch(() => {
+    projectsContainer.innerHTML = /*html*/`<p>Error loading repositories :(</p>`
   })
